1a0372849c423e9eddeb704805c581ae8ea00e1e
Ludmilasilakova <149047976+Ludmilasilakova@users.noreply.github.com>
2023-11-30 21:26:10 +0900

    Add files via upload

--- Задание 6.1.py
+++ Задание 6.1.py
@@ -1,8 +1,10 @@
 def is_palindrome(word):
-  reversed_word = word[::-1]
-  return word == reversed_word
+  word = word.replace(" ", "")
+  word = word.lower()
+  return word == word[::-1]
 word = input("Введите текст:")
 if is_palindrome(word):
   print(True)
+  #если иначе
 else:
   print(False)
